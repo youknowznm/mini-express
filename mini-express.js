f86fade99be693cd9bd287b96ab7d49412fcbe70
youknowznm <znm92@icloud.com>
2018-01-31 21:24:04 +0800

    merge into class

--- mini-express.js
+++ mini-express.js
@@ -1,39 +1,10 @@
 const http = require('http')
 const url = require('url')
 
-const lazy = function* (arr) {
-  yield* arr
-}
-
-const passRouter = (allRoutes, targetMethod, targetPath) => (req, res) => {
-  const lazyRoutes = lazy(allRoutes)
-  ;(function next () {
-    const thisRoute = lazyRoutes.next().value
-    // 1 - 已经遍历结束
-    if (thisRoute === undefined) {
-      res.end(`Cannot ${targetMethod} ${targetPath}`)
-      return
-    }
-    const {method, path, fn} = thisRoute
-    // 2 - 匹配到了符合的路由
-    if ([targetMethod, 'all'].includes(method) && [targetPath, '*'].includes(path)) {
-      fn(req, res)
-      return
-    }
-    // 3 - 匹配到了中间件
-    if (method === 'use' && [targetPath, '/'].includes(path)) {
-      fn(req, res, next)
-      return
-    }
-    next()
-  }())
-}
-
-const HTTP_METHODS = ['get', 'post', 'put', 'delete', 'options', 'all']
-
 class MiniExpress {
   constructor() {
     this.routes = []
+    const HTTP_METHODS = ['get', 'post', 'put', 'delete', 'options', 'all']
     HTTP_METHODS.forEach((method) => {
       this[method] = (path, fn) => {
         this.routes.push({method, path, fn})
@@ -41,10 +12,39 @@ class MiniExpress {
     })
     this.requestListener = this.requestListener.bind(this)
   }
+  getRouter (targetMethod, targetPath) {
+    const lazy = function* (arr) {
+      yield* arr
+    }
+    const lazyRoutes = lazy(this.routes)
+    return (req, res) => {
+      ;(function next () {
+        const thisRoute = lazyRoutes.next().value
+        // 1 - 已经遍历结束
+        if (thisRoute === undefined) {
+          res.end(`Cannot ${targetMethod} ${targetPath}`)
+          return
+        }
+        const {method, path, fn} = thisRoute
+        // 2 - 匹配到了符合的路由
+        if ([targetMethod, 'all'].includes(method) && [targetPath, '*'].includes(path)) {
+          fn(req, res)
+          return
+        }
+        // 3 - 匹配到了中间件
+        if (method === 'use' && [targetPath, '/'].includes(path)) {
+          fn(req, res, next)
+          return
+        }
+        next()
+      }())
+    }
+  }
+  // 定义对所有请求的监听函数，用作 http.createServer() 的参数
   requestListener (req, res) {
     const method = req.method.toLowerCase()
     const pathname = url.parse(req.url).pathname
-    passRouter(this.routes, method, pathname)(req, res)
+    this.getRouter(method, pathname)(req, res)
   }
   listen(port = 4000, host = 'localhost') {
     http
