8e41326da41b3cf1cc9c051869ef0dd896a06915
youknowznm <znm92@icloud.com>
2018-02-01 18:05:25 +0800

    merge into class

--- mini-express.js
+++ mini-express.js
@@ -1,39 +1,46 @@
 const http = require('http')
 const url = require('url')
 
+// 创建一个 MiniExpress 类
 class MiniExpress {
   constructor() {
     this.routes = []
+    // 把常用的 http 方法动词存入一个数组，最后加上 Express 风格的 'all'，可匹配所有方法
     const HTTP_METHODS = ['get', 'post', 'put', 'delete', 'options', 'all']
+    // 遍历方法名的数组，给本类添加同名的成员方法
+    // 该方法接受 path 和 handler 两个参数，在被调用时推入一个路由对象至路由数组
     HTTP_METHODS.forEach((method) => {
-      this[method] = (path, fn) => {
-        this.routes.push({method, path, fn})
+      this[method] = (path, handler) => {
+        this.routes.push({method, path, handler})
       }
     })
+    // 类的实例中的 this 默认不会指向实例本身，修改之（常写 React 的你是不是感觉很亲切）
     this.requestListener = this.requestListener.bind(this)
   }
-  getRouter (targetMethod, targetPath) {
-    const lazy = function* (arr) {
+  // 使用路由迭代器进行路由的遍历
+  getRouterHandler (targetMethod, targetPath) {
+    const generator = function* (arr) {
       yield* arr
     }
-    const lazyRoutes = lazy(this.routes)
+    const routeIterator = generator(this.routes)
     return (req, res) => {
       ;(function next () {
-        const thisRoute = lazyRoutes.next().value
+        const thisRoute = routeIterator.next().value
         // 1 - 已经遍历结束
         if (thisRoute === undefined) {
-          res.end(`Cannot ${targetMethod} ${targetPath}`)
+          res.end(`### Router not found: ${targetMethod} ${targetPath} ###`)
           return
         }
-        const {method, path, fn} = thisRoute
+        const {method, path, handler} = thisRoute
         // 2 - 匹配到了符合的路由
         if ([targetMethod, 'all'].includes(method) && [targetPath, '*'].includes(path)) {
-          fn(req, res)
+          handler(req, res)
           return
         }
         // 3 - 匹配到了中间件
         if (method === 'use' && [targetPath, '/'].includes(path)) {
-          fn(req, res, next)
+          // 传递 next 方法给监听函数，在其内部可进行条件判断，决定是否把执行权交回 routeIterator
+          handler(req, res, next)
           return
         }
         next()
@@ -44,8 +51,9 @@ class MiniExpress {
   requestListener (req, res) {
     const method = req.method.toLowerCase()
     const pathname = url.parse(req.url).pathname
-    this.getRouter(method, pathname)(req, res)
+    this.getRouterHandler(method, pathname)(req, res)
   }
+  // 默认在本地的4000端口监听。服务器就绪后打印 log
   listen(port = 4000, host = 'localhost') {
     http
       .createServer(this.requestListener)
@@ -53,8 +61,8 @@ class MiniExpress {
         console.log(`### Mini-express running at ${host}\:${port}. ###`)
       })
   }
-  use(path, fn) {
-    this.routes.push({method: 'use', path, fn})
+  use(path, handler) {
+    this.routes.push({method: 'use', path, handler})
   }
 }
 
